17d727587a552012fb6f0458172dbaf3c63357e3
Chigozie Oduah <oduahchigozie46@gmail.com>
2025-09-05 10:06:33 +0100

    simplfied a lot of code

--- mash.cpp
+++ mash.cpp
@@ -1,8 +1,10 @@
 #include <csignal>
 #include <cstddef>
 #include <cstdio>
+#include <ctime>
 #include <ctype.h>
 #include <deque>
+#include <sstream>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +13,7 @@
 #include <string>
 #include <termios.h>
 #include <unistd.h>
+#include <vector>
 
 #define PREFIX "\e[2K\r$ %s"
 #define prefix_withb(line, backspace) printf(PREFIX "\e[%dD", line, backspace);
@@ -59,7 +62,8 @@ std::string getcmdline() {
     while(true) {
         char c = getchar();
         if (c == '\n') {
-            printf("\n");
+            if (line.size() > 0)
+                printf("\n");
             break;
         } else if (c == '\x04') {
             raise(SIGINT);
@@ -102,8 +106,7 @@ std::string getcmdline() {
                         backspace = 0;
                         printf(PREFIX, line.c_str());
                     }
-                } else if (c2 == 'C') {
-                    // right
+                } else if (c2 == 'C') { // right
                     if (backspace > 0)
                         backspace--;
 
@@ -112,8 +115,7 @@ std::string getcmdline() {
                     } else {
                         printf(PREFIX, line.c_str());
                     }
-                } else if (c2 == 'D') {
-                    // left
+                } else if (c2 == 'D') { // left
                     if (backspace < line.size())
                         backspace++;
                     prefix_withb(line.c_str(), backspace);
@@ -144,107 +146,19 @@ std::string getcmdline() {
 }
 
 
-size_t parseargs(const char* line, char*** args) {
-    size_t len = strlen(line);
-    size_t allocated_size = 5;
-    *args = (char**) malloc(allocated_size * sizeof(char*));
-    size_t added = 0;
+std::vector<std::string> parseargs(std::string line) {
+    std::vector<std::string> args;
+    std::stringstream liness(line);
+    std::string tmp;
 
-    size_t allocated_text_size = 10;
-    char* collected_arg = (char*) malloc(allocated_text_size * sizeof(char));
-    size_t collected_arg_size = 0;
-    memset(collected_arg, '\0', allocated_text_size);
-
-    bool escape = false;
-    bool quotes = false;
-
-    for (size_t i = 0; i < len + 1; i++) {
-        if (line[i] == '\\') {
-            escape = true;
-            continue;
-        }
-
-        if (line[i] == '"' && !escape) {
-            quotes = !quotes;
-            continue;
-        }
-
-        if (added == allocated_size) {
-            allocated_size *= 2;
-            *args = (char**) realloc(*args, allocated_size * sizeof(char*));
-        }
-
-        if (collected_arg_size == allocated_text_size) {
-            allocated_text_size *= 2;
-            collected_arg = (char*) realloc(collected_arg, allocated_text_size * sizeof(char));
-        }
-
-        if ((line[i] == ' ' && !escape && !quotes) || line[i] == '\0') {
-            if (collected_arg_size > 0) {
-                collected_arg[collected_arg_size] = '\0';
-                collected_arg_size++;
-                (*args)[added] = (char*) realloc(collected_arg, collected_arg_size * sizeof(char));
-                added++;
-
-                if (line[i] == ' ') {
-                    allocated_text_size = 10;
-                    collected_arg = (char*) malloc(allocated_text_size * sizeof(char));
-                    collected_arg_size = 0;
-                    memset(collected_arg, '\0', allocated_text_size);
-                }
-            } else if (line[i] == '\0') {
-                free(collected_arg);
-            }
-
-            continue;
-        }
-
-        if (escape) {
-            switch (line[i]) {
-                case 'n':
-                    collected_arg[collected_arg_size] = '\n';
-                    collected_arg_size++;
-                    break;
-
-                case 'b':
-                    collected_arg[collected_arg_size] = '\b';
-                    collected_arg_size++;
-                    break;
-
-                default:
-                    collected_arg[collected_arg_size] = line[i];
-                    collected_arg_size++;
-                    break;
-            }
-            escape = false;
-        } else {
-            collected_arg[collected_arg_size] = line[i];
-            collected_arg_size++;
+    while (std::getline(liness, tmp, ' ')) {
+        if (tmp.size() > 0) {
+            args.push_back(tmp);
         }
     }
 
-
-    if (added > 0) {
-        *args = (char**) realloc(*args, added * sizeof(char*));
-    } else {
-        free(*args);
-        *args = NULL;
-    }
-
-    return added;
+    return args;
 }
-
-
-void cleanup_args(char*** args, size_t count) {
-    if (*args != NULL) {
-        for (int i = 0; i < count; i++) {
-            free((*args)[i]);
-        }
-        free(*args);
-        *args = NULL;
-    }
-}
-
 
 struct termios original, noecho;
 
@@ -277,13 +191,10 @@ int main(int argc, char** argv) {
             if (line.size() == 0)
                 printf("\n");
 
-            char** args = NULL;
-            size_t cli_len = parseargs(line.c_str(), &args);
+            std::vector<std::string> args = parseargs(line);
 
-            if (args != NULL)
-                printf("First argument: %s\n", args[0]);
-
-            cleanup_args(&args, cli_len);
+            if (!args.empty())
+                printf("First argument: %s\n", args[0].c_str());
         }
     }
 
